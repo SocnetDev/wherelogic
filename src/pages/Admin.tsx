0ac9c271cddad795fc6042791df7122cb82f5a6e
aa.adilov <aa.adilov@tensor.ru>
2020-03-03 16:53:46 +0300

    Добавил в админку настройку команд-участников (без стилизации и БЛ)

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
+
+type CommandsListProps = {
+    count: number;
+}
+
+type CommandProps = {
+    name: string;
+}
+
+function Command(props: CommandProps) {
+    const [name, setName] = useState<any>(props.name || '');
+
+    return (
+        <li>
+            <div>{name}</div>
+            <label>Название команды <input type="text"
+                                           value={name}
+                                           onChange={(e: ChangeEvent<HTMLInputElement>) => {setName(e.target.value)}}/>
+            </label>
+            <label>Очки команды <input type="number" min="0"/></label>
+        </li>
+    );
+}
+
+function CommandsList(props: CommandsListProps) {
+    const commands = Array.from({length: props.count}, () => null);
+    const commandsItems = commands.map((number: any, index: number) =>
+        <Command name={`Команда  ${index + 1}`} key={index}/>
+    );
+    return (
+        <ul>{commandsItems}</ul>
+    );
+}
 
 export function Admin() {
-    // Тут короче то что видит ведущий, а именно кнопка переключения вопроса, правильный ответ, возможно подсказки
-    return <h2>Admin</h2>;
+    // Начальный экран ведущего
+    return (
+        <div>
+            <h2>Настройки текущей игры</h2>
+            <CommandsList count={3} />
+        </div>
+    );
 }
